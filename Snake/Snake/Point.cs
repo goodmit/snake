18cc61a7964f1e6d4ec7a79f7ee4b4425538cb6b
goodmit <dymbers@mail.ru>
2017-01-10 16:45:06 +0300

    Создан класс Snake, наследуемый от Figure. Изменен класс Point.

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -21,9 +21,40 @@ namespace Snake
             sym = _sym;
         }
 
+        public Point(Point p)
+        {
+            x = p.x;
+            y = p.y;
+            sym = p.sym;
+        }
+
         public void Draw() {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            switch(direction)
+            {
+                case Direction.RIGHT:
+                    x += offset;
+                    break;
+                case Direction.LEFT:
+                    x -= offset;
+                    break;
+                case Direction.UP:
+                    y -= offset;
+                    break;
+                case Direction.DOWN:
+                    y += offset;
+                    break;
+            }
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym;
         }
     }
 }
